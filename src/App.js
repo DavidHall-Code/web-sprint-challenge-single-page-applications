60e4b85c189b1ed1784fbf6684f342af22ecae6b
DavidHall-Code <david_m_hall@outlook.com>
2020-11-13 11:57:11 -0500

    Set up app.js, going to work on Home.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { Route, Link, Switch } from 'react-router-dom'
+//import Form from './Components/Form'
+//import HomePage from './Components/Home'
+import image from './Components/Assets/Pizza.jpg'
+import './App.css'
 
-const App = () => {
+
+function App () {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
-};
-export default App;
+    <div className='App'>
+      <header>
+        <h1 className='PageName'>Dragon Fire Pizza</h1>
+        <div className='nav-links'>
+          <Link className='links' to='/'>Home</Link>
+          <Link className="links" to="/pizza">Order Now</Link>
+        </div>
+      </header>
+
+      {/* <Switch>
+        <Route path='/pizza'>
+          <Form />
+        </Route>
+
+        <Route path='/'>
+          <HomePage />
+        </Route>
+      </Switch> */}
+    </div>
+  )
+}
+
+export default App
